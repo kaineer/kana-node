60ec40e4e5f4b167db55d0aab750acea67a58d75
Sergey Kluchkovsky <kaineer@gmail.com>
2024-07-03 17:29:42 +0500

    Add test for kana

--- test/kana.test.js
+++ test/kana.test.js
@@ -1,3 +1,16 @@
 //
 
+const { test } = require("uvu");
+const assert = require("uvu/assert");
+
 const { romaji } = require("..");
+
+test("romaji to hiragana", () => {
+  assert.is(romaji("sakana").hiragana(), "さかな");
+});
+
+test("romaji to katakana", () => {
+  assert.is(romaji("toire").katakana(), "トイレ");
+});
+
+test.run();
